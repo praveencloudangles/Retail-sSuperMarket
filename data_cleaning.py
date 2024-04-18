cbb3914d95f4e5c31087ffd82d3c3bd427fd2662
praveencloudangles <112871831+praveencloudangles@users.noreply.github.com>
2024-04-18 13:01:48 +0530

    Update data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,5 +1,6 @@
 print("data cleaning-------------")
 from data_analysis import data_analysis
+from sklearn.preprocessing import LabelEncoder
 
 def data_cleaning():
     data = data_analysis()
@@ -19,6 +20,15 @@ def data_cleaning():
     data['Region'] = data['Region'].replace(['West', 'East', 'Central', 'South'], [0, 1, 2, 3])
 
     data['Category'] = data['Category'].replace(['Office Supplies', 'Furniture', 'Technology'], [0, 1, 2])
+
+    label_encoder = LabelEncoder()
+
+    # Specify columns to encode
+    columns_to_encode = ['City', 'State', 'Sub-Category']
+
+    # Apply label encoding to specified columns
+    for column in columns_to_encode:
+        data[column] = label_encoder.fit_transform(data[column])
 
     return data
 data_cleaning()
