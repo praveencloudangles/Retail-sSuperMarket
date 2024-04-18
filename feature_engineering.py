ede1cb2ceff3e3a9d61fafe354f9ea2559298ee3
praveencloudangles <112871831+praveencloudangles@users.noreply.github.com>
2024-04-18 13:02:31 +0530

    Update feature_engineering.py

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,19 +1,9 @@
 print("feature engineer-------------------")
-from data_cleaning import datavisualization
-from sklearn.preprocessing import LabelEncoder
+from datavisualization import data_vis
 import pandas as pd
 
 def fea_eng():
-    data = datavisualization()
-
-    label_encoder = LabelEncoder()
-
-    # Specify columns to encode
-    columns_to_encode = ['City', 'State', 'Sub-Category']
-
-    # Apply label encoding to specified columns
-    for column in columns_to_encode:
-        data[column] = label_encoder.fit_transform(data[column])
+    data = data_vis()
 
 
     data.to_csv("Retail_SuperMarket.csv", index=False)
